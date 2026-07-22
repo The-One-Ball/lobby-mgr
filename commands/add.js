ddfffd45a793799f1578968ff92eb1eeb4f7ea39
The-One-Ball <bk121619@gmail.com>
2026-07-22 15:13:02 -0400

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { buildLobbyEmbed, buildLobbyComponents } = require('../embedBuilder');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,10 +15,9 @@ module.exports = {
     const player = interaction.options.getUser('player');
 
     // Find the lobby where this user is the host
-    const hostLobby = lobbyManager.getLobbyByHost(interaction.user.id);
+    const lobby = lobbyManager.getLobbyByHost(interaction.user.id);
 
-    // If the user is not a host of any lobby
-    if (!hostLobby) {
+    if (!lobby) {
       return interaction.reply({
         content: 'You are not the host of any active lobby.',
         ephemeral: true
@@ -25,14 +25,33 @@ module.exports = {
     }
 
     // Add player if not already in lobby
-    if (!hostLobby.players.includes(player.id)) {
-      hostLobby.players.push(player.id);
-      hostLobby.currentCount = hostLobby.players.length;
+    if (!lobby.players.includes(player.id)) {
+      lobby.players.push(player.id);
+      lobby.currentCount = lobby.players.length;
     }
 
+    // Update the lobby embed + buttons
+    try {
+      const embed = buildLobbyEmbed(lobby);
+      const components = buildLobbyComponents(lobby);
+
+      const channel = await interaction.guild.channels.fetch(lobby.channelId);
+      const msg = await channel.messages.fetch(lobby.messageId);
+
+      await msg.edit({ embeds: [embed], components });
+    } catch (err) {
+      console.error("Failed to update lobby message:", err);
+
+      return interaction.reply({
+        content: `Added ${player.username}, but I couldn't update the lobby message due to missing permissions.`,
+        ephemeral: true
+      });
+    }
+
+    // Success
     return interaction.reply({
-      content: `Added ${player.username} to lobby ${hostLobby.code}.`,
-      ephemeral: false
+      content: `Added ${player.username} to lobby ${lobby.code}.`,
+      ephemeral: true
     });
   }
 };
