06e7a06058b890925536e1401eb953bc8fdefc77
The-One-Ball <bk121619@gmail.com>
2026-07-22 15:02:54 -0400

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -3,36 +3,36 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('add')
-    .setDescription('Add a player to the lobby')
+    .setDescription('Add a player to your lobby (host only)')
     .addUserOption(opt =>
       opt.setName('player')
         .setDescription('Player to add')
-        .setRequired(true)
-    )
-    .addStringOption(opt =>
-      opt.setName('lobby')
-        .setDescription('Lobby ID')
         .setRequired(true)
     ),
 
   async execute(interaction, lobbyManager) {
     const player = interaction.options.getUser('player');
-    const lobbyId = interaction.options.getString('lobby');
 
-    const lobby = lobbyManager.getLobby(lobbyId);
-    if (!lobby) {
-      return interaction.reply({ content: 'Lobby not found.', ephemeral: true });
+    // Find the lobby where this user is the host
+    const hostLobby = lobbyManager.getLobbyByHost(interaction.user.id);
+
+    // If the user is not a host of any lobby
+    if (!hostLobby) {
+      return interaction.reply({
+        content: 'You are not the host of any active lobby.',
+        ephemeral: true
+      });
     }
 
-    if (interaction.user.id !== lobby.hostId) {
-      return interaction.reply({ content: 'Only the host can add players.', ephemeral: true });
+    // Add player if not already in lobby
+    if (!hostLobby.players.includes(player.id)) {
+      hostLobby.players.push(player.id);
+      hostLobby.currentCount = hostLobby.players.length;
     }
 
-    if (!lobby.players.includes(player.id)) {
-      lobby.players.push(player.id);
-      lobby.currentCount = lobby.players.length;
-    }
-
-    return interaction.reply({ content: `Added ${player.username} to lobby ${lobby.code}.`, ephemeral: true });
+    return interaction.reply({
+      content: `Added ${player.username} to lobby ${hostLobby.code}.`,
+      ephemeral: false
+    });
   }
 };
